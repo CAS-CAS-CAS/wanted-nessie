663753d3837792b8127f88a4d8879bdcb15f983c
CAS-CAS-CAS <247Adventurer@gmail.com>
2022-07-26 21:27:41 -0700

    abstracted functions in  maploader

--- public/MapLoader.js
+++ public/MapLoader.js
@@ -1,42 +1,45 @@
 
 function main(){
     var map = constructMap();
+    var coord = [-111.9234527085402, 33.418017665847174];
+    
+    map.on('click', function(event){
+        var layer = makePinLayer(event.coordinate);
+        map.addLayer(layer);
+    })
+    
 }
 
-console.log(window.location);
 
 function constructMap(){
-/*console.log("LIFT OFF!");
-console.log("LIVE LOOPIN, BABY!!\nAlmond milk hit")*/
-var centerCoor = [-111.9234527085402, 33.418017665847174];
-var map = new ol.Map({
-target: 'map',
-layers: [
-    new ol.layer.Tile({
-        source: new ol.source.OSM()
+    const centerCoor = [-111.9234527085402, 33.418017665847174];
+    var map = new ol.Map({
+        target: 'map',
+        layers: [new ol.layer.Tile({source: new ol.source.OSM()})],
+        view: new ol.View({
+        center: ol.proj.fromLonLat(centerCoor), //[-12460306.871548783, 3951536.3379208655]
+        minZoom: 15,
+        zoom: 17})
+    });
+    return map;
+};
+
+function makePinLayer(coords){
+    var layer = new ol.layer.Vector({
+        source: new ol.source.Vector({
+            features: [
+                new ol.Feature({
+                    geometry: new ol.geom.Point(ol.proj.fromLonLat(coords))
+                })
+            ]
         })
-    ],
-view: new ol.View({
-center: ol.proj.fromLonLat(centerCoor), //[-12460306.871548783, 3951536.3379208655]
-minZoom: 15,
-zoom: 17
-    })
-});
-//marker -- remove  eventually
+    });
+    return layer;
+};
 
-var layer = new ol.layer.Vector({
-    source: new ol.source.Vector({
-        features: [
-            new ol.Feature({
-                geometry: new ol.geom.Point(ol.proj.fromLonLat(centerCoor))
-            })
-        ]
-    })
-});
-map.addLayer(layer);
-
+main();
 //Popups
-var container = document.getElementById('popup');
+/*var container = document.getElementById('popup');
 var content = document.getElementById('popup-content');
 var closer = document.getElementById('popup-closer');
 
@@ -83,25 +86,6 @@ map.on('singleclick', function (event) {
         });
         map.addLayer(layer);
     }});
-    return map;
-};
+    */
 
-/*
-  function setPoint(event, map){
-    //console.log(map);
-    //console.log(ol.proj.transform(event.coordinate, 'EPSG:3857', 'EPSG:4326'));
-    event.coordinate;
-    var layer = new ol.layer.Vector({
-        source: new ol.source.Vector({
-            features: [
-                new ol.Feature({
-                    geometry: new ol.geom.Point([-111.9234527085402, 33.418017665847174])
-                })
-            ]
-        })
-    });
-    console.log(layer);
-    map.addLayer(layer);
-  }
-*/
-  main();
+
